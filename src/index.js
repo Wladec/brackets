bd7a0219cc9c3cad801fc9a2f75802a413ded321
Wladec <vladick.tsemenko@gmail.com>
2019-03-24 23:15:48 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let temp;
+
+    for (let i = 0; i < str.length; i++){
+        for (let j = 0; j < bracketsConfig.length; j++){
+            if (str[i] === bracketsConfig[j][0]) stack.push(i);
+            else temp = stack.pop(i);
+            if (temp === str[i+1]) return false;
+            else return true;
+        }
+    }
+
 }
